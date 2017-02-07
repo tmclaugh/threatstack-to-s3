f67021194f5658d96d774924a536d192461391df
Tom McLaughlin <tom.mclaughlin@threatstack.com>
2017-02-06 22:27:14 -0500

    Let's not blindly reveal every exception.
    We could end up leaking information we did not intend to.  Currently we're
    confident enough with S3ClientError and ThreatStackError but we'll fix those
    in another read of changes.

--- app/errors.py
+++ app/errors.py
@@ -1,11 +1,14 @@
 '''
 Application error handlers.
 '''
+from app.models.s3 import S3ClientError
+from app.models.threatstack import ThreatStackError
 from flask import Blueprint, jsonify
 
 errors = Blueprint('errors', __name__)
 
-@errors.app_errorhandler(Exception)
+@errors.app_errorhandler(S3ClientError)
+@errors.app_errorhandler(ThreatStackError)
 def handle_threatstack_error(error):
     status_code = 500
     success = False
@@ -19,3 +22,16 @@ def handle_threatstack_error(error):
 
     return jsonify(response), status_code
 
+@errors.app_errorhandler(Exception)
+def handle_unexpected_error(error):
+    status_code = 500
+    success = False
+    response = {
+        'success': success,
+        'error': {
+            'type': 'UnexpectedException',
+            'message': 'An unexpected error has occurred.'
+        }
+    }
+
+    return jsonify(response), status_code
